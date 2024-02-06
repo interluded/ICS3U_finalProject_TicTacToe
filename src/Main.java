49fdbafcef1dbd3eb7bd1ab6a39671fba47349be
marclmao <119444616+marclmao@users.noreply.github.com>
2024-02-06 15:47:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame();
+        frame.setBounds(0,0,900,900);
+        frame.setTitle("Tic Tac Woah, by Interluded");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        tttGame game = new tttGame();
+        frame.add(game);
+        frame.setVisible(true);
+    }
+}
---
+++ src/tttGame.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import javax.imageio.*;
+import java.io.*;
+public class tttGame extends JPanel implements MouseListener
+{
+    Image title;
+    Image background;
+    Image xImage;
+    Image OImage;
+    int screen = 1;
+    int players;
+
+
+
+
+
+
+    public tttGame(){
+        addMouseListener(this);
+
+    }
+
+
+
+
+
+
+
+    public void mouseClicked(MouseEvent e){}
+    public void mousePressed(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    public void mouseEntered(MouseEvent e){}
+    public void mouseExited(MouseEvent e){}
+}
