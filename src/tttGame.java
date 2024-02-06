3fa41cb5e78e704ff50aad10a8a3d0a611ea82ed
marclmao <119444616+marclmao@users.noreply.github.com>
2024-02-06 16:14:56 -0500

    mouseclick now works on p2, just switches to screen2 which is just a full red screen
    PLAN TO ADD:
    
    p2p chat with netcat(probably wont work at school due to network restrictions)
    Music
    More photos
    yea

--- src/tttGame.java
+++ src/tttGame.java
@@ -53,16 +53,29 @@ public class tttGame extends JPanel implements MouseListener
 
         //draw 2p button
         g.setColor(Color.RED);
-        g.fillRect(450,600,300,100);
+        g.fillRect(500,600,300,100);
         g.setColor(Color.WHITE);
-        g.drawString("2P START",550,655);
+        g.drawString("2P START",610,655);
     }
     public void drawBoard(Graphics g){
-
+g.setColor(Color.RED);
+g.fillRect(0,0,900,900);
     }
 
     public void mouseClicked(MouseEvent e){}
-    public void mousePressed(MouseEvent e){}
+    public void mousePressed(MouseEvent e){
+        int x = e.getX();
+        int y = e.getY();
+        // on title screen
+        if(screen == 1) {
+           if(x >= 500 && x <=800 && y>= 600 && y <= 700){
+        System.out.println("DEBUG");
+        screen = 2;
+        players = 2;
+           }
+        }
+    repaint();
+    }
     public void mouseReleased(MouseEvent e){}
     public void mouseEntered(MouseEvent e){}
     public void mouseExited(MouseEvent e){}
